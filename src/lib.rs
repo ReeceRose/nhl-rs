50f504a12a114931af10a5b27d95736832ca970b
Reece Rose <reecerose18@gmail.com>
2023-10-23 21:40:29 -0400

    Awards and Conferences client

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,20 @@
+pub mod awards;
+pub mod conferences;
+pub(crate) mod http;
 
+use awards::AwardsClient;
+use conferences::ConferencesClient;
 
+pub struct Client {
+    pub awards: AwardsClient,
+    pub confrences: ConferencesClient,
+}
+
+impl Client {
+    pub fn new() -> Self {
+        Client {
+            awards: AwardsClient::new(),
+            confrences: ConferencesClient::new(),
+        }
+    }
+}
